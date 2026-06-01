ce21abb63976728bcfc0d21223ecf4d97aeff811
Daniel-Easterday13 <easterdayd@patriots.cf.edu>
2026-06-01 14:15:01 -0400

    Update momentum2.py
    while doing the assignment it was giving errors, in the print for kinetic energy. it wanted the mass and velocity  to have float then input.

--- Chapter 2/momentum2.py
+++ Chapter 2/momentum2.py
@@ -6,8 +6,8 @@
 import math
 
 
-mass = input("Enter the mass of the object, in kilograms: ")
-velocity = input("Enter the velocity of the object, in meters per second: ")
+mass = float(input("Enter the mass of the object, in kilograms: "))
+velocity = float(input("Enter the velocity of the object, in meters per second: "))
 
 momentum = float(mass) * float(velocity)
 
